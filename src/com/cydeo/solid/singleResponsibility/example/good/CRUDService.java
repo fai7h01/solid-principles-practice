5dc7da35d7970faccafdc4abdaf418c1be685a15
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-12 21:35:24 +0400

    Single responsibility -> good and bad examples

---
+++ src/com/cydeo/solid/singleResponsibility/example/good/CRUDService.java
@@ -0,0 +1,24 @@
+package com.cydeo.solid.singleResponsibility.example.good;
+
+public class CRUDService {
+
+    public static void create(){
+        // Create a User
+        // Send User to database related part
+    }
+
+    public static void read(){
+        // Read a User
+        // Send User to database related part
+    }
+
+    public static void update(){
+        // Update a User
+        // Send User to database related part
+    }
+
+    public static void delete(){
+        // Delete a User
+        // Send User to database related part
+    }
+}
