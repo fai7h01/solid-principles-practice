4177b77fb18a05da929300920d033b8d63d20d3c
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-12 21:08:28 +0400

    source code dependency example

---
+++ src/com/cydeo/X.java
@@ -0,0 +1,10 @@
+package com.cydeo;
+
+public class X {
+    public static void main(String[] args) {
+
+        Z z = new A();
+        z.method();
+
+    }
+}
---
+++ src/com/cydeo/Z.java
@@ -0,0 +1,5 @@
+package com.cydeo;
+
+public interface Z {
+    void method();
+}
