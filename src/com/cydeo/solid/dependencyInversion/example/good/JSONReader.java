80596152c77b5795a3dc5a25b73bc078d07ec455
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-13 20:55:30 +0400

    dependency inversion good example

---
+++ src/com/cydeo/solid/dependencyInversion/example/good/JSONReader.java
@@ -0,0 +1,18 @@
+package com.cydeo.solid.dependencyInversion.example.good;
+
+public class JSONReader implements Reader{
+
+    /*
+         Example JSON:
+
+         {
+               "username": "mikesmith"
+         }
+
+     */
+
+    @Override
+    public String getUsername() {
+        return "{\"username\": \"mikesmith\"}";
+    }
+}
