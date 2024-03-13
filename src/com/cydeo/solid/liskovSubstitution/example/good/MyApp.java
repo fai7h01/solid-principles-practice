c364228bba247a133597f121b6314b356f1f07fa
fai7h01 <l.varsimashvil10@gmail.com>
2024-03-13 16:41:11 +0400

    Liskov substitution principle good and bad examples

---
+++ src/com/cydeo/solid/liskovSubstitution/example/good/MyApp.java
@@ -0,0 +1,33 @@
+package com.cydeo.solid.liskovSubstitution.example.good;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(4);
+        rectangle.setWidth(6);
+
+        Square square = new Square();
+        square.setSide(5);
+
+//        List<Rectangle> rectangles = Arrays.asList(rectangle, square);
+
+//        changeHeightAndWidth(rectangles);
+
+
+    }
+
+    public static void changeHeightAndWidth(List<Rectangle> rectangles) {
+
+        for (Rectangle rectangle : rectangles) {
+            rectangle.setHeight(5);   // Rectangle H -> 5  - Square H, W -> 5
+            rectangle.setWidth(6);    // Rectangle W -> 6  - Square W, H -> 6
+            System.out.println(rectangle.getArea() == 30);   // Rectangle Area -> 30
+        }                                                    // Square Area -> 36
+
+    }
+
+}
